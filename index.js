ba6190d37fda22b22b668657e064fbc16648c259
john <john joshua>
2024-06-09 22:11:18 +0530

    1.3

--- index.js
+++ index.js
@@ -6,9 +6,12 @@ const { prototype } = require('module');
 const app=express();
 app.use(express.json());
 app.use(cors())
-app.use('/',express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/public/index.html'));
+  });
+  
 app.use('/api',route);
 
 const port=process.env.X_ZOHO_CATALYST_LISTEN_PORT;
